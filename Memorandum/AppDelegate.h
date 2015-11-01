274090a7724b5c1012f836a3239545cf664ee1f7
demo5 <1505257360@qq.com>
2015-11-01 11:10:37 +0800

    Initial Commit

---
+++ Memorandum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memorandum
+//
+//  Created by YoKing on 15/11/1.
+//  Copyright © 2015年 YQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memorandum/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Memorandum
+//
+//  Created by YoKing on 15/11/1.
+//  Copyright © 2015年 YQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
